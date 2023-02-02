6ad2d69e3623893a9dd3f4014def2a57c847be81
SergioBalca <SergioBalca@github.com>
2023-02-02 12:24:26 -0500

    Agregar proyecto con dependencias administradas con el spring Initializr

---
+++ src/main/java/com/balcazar/sergio/proyectosistematransporte/ProyectoSistematransporteApplication.java
@@ -0,0 +1,13 @@
+package com.balcazar.sergio.proyectosistematransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSistematransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSistematransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balcazar/sergio/proyectosistematransporte/ProyectoSistematransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balcazar.sergio.proyectosistematransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSistematransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
